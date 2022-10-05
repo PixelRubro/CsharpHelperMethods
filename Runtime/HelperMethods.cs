99021d070d3411230b4f664a110c74cc484f086b
msavioti <angelosavioti@gmail.com>
2022-10-05 19:36:14 -0300

    feat: created asset

---
+++ Runtime/HelperMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SoftBoiledGames.CsharpHelperMethods
+{
+    public static class CsharpHelper
+    {
+        // Author: Soft Boiled Games
+        /// <summary>
+        /// Swap the two values provided between themselves.
+        /// </summary>
+        public static void Swap<T>(ref T v1, ref T v2)
+        {
+            T temp = v1;
+            v1 = v2;
+            v2 = temp;
+        }
+
+        // Author: Soft Boiled Games
+        /// <summary>
+        /// Returns the smallest value between <paramref name="v1"/>
+        /// and <paramref name="v2"/>.
+        /// </summary>
+        /// <typeparam name="T">A value implementing IComparable interface.</typeparam>
+        public static T Min<T>(T v1, T v2) where T : IComparable<T>
+        {
+            return v1.CompareTo(v2) < 0 ? v1 : v2;
+        }
+
+        // Author: Soft Boiled Games
+        /// <summary>
+        /// Returns the greatest value between <paramref name="v1"/>
+        /// and <paramref name="v2"/>.
+        /// </summary>
+        /// <typeparam name="T">A value implementing IComparable interface.</typeparam>
+        public static T Max<T>(T v1, T v2) where T : IComparable<T>
+        {
+            return v1.CompareTo(v2) > 0 ? v1 : v2;
+        }
+    }
+}
